ba140bbbdd0ee2f070151fef66d11f7637fae204
juena <jferdous@MBP512-JFERDOUS-0217.local>
2017-04-27 17:40:43 +0100

    Chefspec tests added

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -2,4 +2,6 @@ mysql_db 'Creating MySQL DB' do
   db_user node['aps-db']['username']
   db_pass node['aps-db']['password']
   db_name node['aps-db']['database_name']
+
+  action :run
 end
---
+++ spec/unit/recipes/mysql_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'chefspec'
+
+describe 'aps-db::mysql' do
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new
+    runner.converge(described_recipe)
+  end
+
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
+
+  it 'Creating MySQL DB' do
+    expect(
+      db_user: 'alfresco',
+      db_pass: 'alfresco',
+      db_name: 'process'
+    )
+  end
+end
